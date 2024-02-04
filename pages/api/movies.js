cbb201c2899dcb213925b302d61c29b8dcbc6b63
Gilbert Ssenyonjo <gssenyonjo@gmail.com>
2024-02-04 21:17:45 +0300

    implemented a page and an api for reading movies

---
+++ pages/api/movies.js
@@ -0,0 +1,23 @@
+import clientPromise from '../../lib/mongodb'
+
+export default async (req, res) => {
+    try {
+        const client = await clientPromise;
+        const db = client.db('sample_mflix')
+
+        const movies = await db 
+            .collection('movies')
+            .find({})
+            .sort({ metacritic: -1})
+            .limit(20)
+            .toArray()
+
+        console.log(movies)
+
+        res.json(movies)
+    }
+    catch(e) {
+        console.error(e)
+        throw e;
+    }
+}
